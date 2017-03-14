c48171b8b119a809ed073ba5740a103ed8d8ab2f
harroyo0610 <humberto-_-lzc@hotmail.com>
2017-03-14 13:32:52 -0600

    Database created

---
+++ db/migrate/20170314190142_create_reservations.rb
@@ -0,0 +1,14 @@
+class CreateReservations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reservations do |t|
+      t.belongs_to :user, index: true
+      t.float   :total
+      t.string  :name
+      t.string  :address
+      t.date    :date
+      t.string  :time
+
+      t.timestamps
+    end
+  end
+end
