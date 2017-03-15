88419e0aa056a86dbaa0f490ba14c131c83226ce
armando montoya <jamontoya2@hotmail.com>
2017-03-15 17:23:10 -0600

    sign-up, log-in, password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.force_ssl = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -54,12 +54,27 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "all_party_#{Rails.env}"
+  # config.active_job.queue_name_prefix = "sample_app_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   #####
+   config.action_mailer.raise_delivery_errors = true
+   config.action_mailer.delivery_method = :smtp
+   #config.action_mailer.perform_deliveries = true
+   host = '.herokuapp.com'
+   config.action_mailer.default_url_options = { host: host }
+   ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => '.heroku.com',
+    :enable_starttls_auto => true
+  }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
