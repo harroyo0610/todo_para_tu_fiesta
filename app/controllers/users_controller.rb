a4142baed63df11950818a13cc00c110fd56999e
armando montoya <jamontoya2@hotmail.com>
2017-03-16 17:42:31 -0600

    ok pass and password reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "profile updated"
-      redirect_to @user
+      redirect_to root_url
     else
       render 'edit'  
     end
